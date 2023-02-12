938fbf75f1a75f83b2287bb78137293e793f1b48
Nguyen Hoang Khang <hoangkhang12789@gmail.com>
2023-02-13 02:02:07 +0700

    Add NotificationService

--- Project.NotificationService/Program.cs
+++ Project.NotificationService/Program.cs
@@ -1,6 +1,3 @@
-using Project.Core.AWS;
-using Project.Core.Caching;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddAWSS3Bucket(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Project.TestService/Controllers/TestController.cs
+++ Project.TestService/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Project.Common.Enum;
 using Project.Common.Security;
 using Project.Core.AWS;
+using System.Security.Policy;
 
 namespace Project.TestService.Controllers
 {
@@ -11,36 +12,39 @@ namespace Project.TestService.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        private readonly IAmazonS3Bucket bucket;
+        //    private readonly IAmazonS3Bucket bucket;
 
-        public TestController(IAmazonS3Bucket bucket)
-        {
-            this.bucket = bucket;
-        }
+        //    public TestController(IAmazonS3Bucket bucket)
+        //    {
+        //        this.bucket = bucket;
+        //    }
 
-        [HttpPost]
-        public async Task<IActionResult> Upload( IFormFile file)
-        {
-            var x = await bucket.UploadFileAsync(file, FileType.Image);
-            return Ok(x);
-        }
-        [HttpGet]
-        public async Task<IActionResult> GetUrl(string  key)
-        {
-            var x = await bucket.GetFileAsync(key);
-            return Ok(x);
-        }
-        [HttpDelete]
-        public async Task<IActionResult> DeleteFile(string key)
-        {
-            var x = await bucket.DeleteFileAsync(key);
-            return Ok(x);
-        }
+        //    [HttpPost]
+        //    public async Task<IActionResult> Upload( IFormFile file)
+        //    {
+        //        var x = await bucket.UploadFileAsync(file, FileType.Image);
+        //        return Ok(x);
+        //    }
+        //    [HttpGet]
+        //    public async Task<IActionResult> GetUrl(string  key)
+        //    {
+        //        var x = await bucket.GetFileAsync(key);
+        //        return Ok(x);
+        //    }
+        //    [HttpDelete]
+        //    public async Task<IActionResult> DeleteFile(string key)
+        //    {
+        //        var x = await bucket.DeleteFileAsync(key);
+        //        return Ok(x);
+        //    }
         [HttpGet]
         public IActionResult PaswordGenerate(string password)
         {
             var x = Cryptography.EncryptPassword(password);
-            return Ok(x);
+            var z = BitConverter.ToString(x.Salt).Replace("-", string.Empty);
+            var salt = "0x" + z;
+            var result = new { hash = x.Hash, salt = salt };
+            return Ok(result);
         }
     }
 }
