def3ae56d0b89d6c3a7fb5ae20f0e08084993283
Nguyen Hoang Khang <hoangkhang12789@gmail.com>
2023-02-23 22:14:13 +0700

    add Cors Api Gateways

--- Project.APIGateways/Program.cs
+++ Project.APIGateways/Program.cs
@@ -1,13 +1,15 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-
+using Project.Core.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
                                 .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+var CorsName = "Eclinic";
+builder.Services.AddMyCors(CorsName);
 var app = builder.Build();
-
+app.UseCors(CorsName);
 await app.UseOcelot();
 
 app.Run();
