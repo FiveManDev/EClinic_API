1abf3baae738ad88f8da1e62fb02e5f8f210a996
Nguyen Hoang Khang <hoangkhang12789@gmail.com>
2023-03-19 01:40:14 +0700

    add sh for linux

---
+++ bash/ForumServiceForLinux.sh
@@ -0,0 +1,4 @@
+.\linuxmongoimport --db ForumService --collection Post --file ../Database/DataMongo/Post.json --jsonArray --uri "mongodb://localhost:27018/"
+.\linuxmongoimport --db ForumService --collection Answer --file ../Database/DataMongo/Answer.json --jsonArray --uri "mongodb://localhost:27018/"
+.\linuxmongoimport --db ForumService --collection Comment --file ../Database/DataMongo/Comment.json --jsonArray --uri "mongodb://localhost:27018/"
+.\linuxmongoimport --db ForumService --collection Hashtag --file ../Database/DataMongo/Hashtag.json --jsonArray --uri "mongodb://localhost:27018/"
