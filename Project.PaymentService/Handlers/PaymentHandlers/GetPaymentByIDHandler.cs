e1a5956b5221ed1de9feee9f1ec4c054e512b794
Nguyen Hoang Khang <hoangkhang12789@gmail.com>
2023-06-20 20:34:17 +0700

    update grpc

--- Project.PaymentService/Handlers/PaymentHandlers/GetPaymentByIDHandler.cs
+++ Project.PaymentService/Handlers/PaymentHandlers/GetPaymentByIDHandler.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using Grpc.Net.Client;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Project.Common.Response;
 using Project.Core.Logger;
 using Project.PaymentService.Model;
+using Project.PaymentService.Protos;
 using Project.PaymentService.Queries;
 using Project.PaymentService.Repository.PaymentRepositories;
 
@@ -14,12 +16,16 @@ namespace Project.RefundService.Handlers.RefundHandlers
         private readonly IPaymentRepository paymentRepository;
         private readonly ILogger<GetPaymentByIDHandler> logger;
         private readonly IMapper mapper;
-
-        public GetPaymentByIDHandler(IPaymentRepository paymentRepository, ILogger<GetPaymentByIDHandler> logger, IMapper mapper)
+        private readonly ProfileService.ProfileServiceClient client;
+        public GetPaymentByIDHandler(IConfiguration configuration, IPaymentRepository paymentRepository, ILogger<GetPaymentByIDHandler> logger, IMapper mapper)
         {
             this.paymentRepository = paymentRepository;
             this.logger = logger;
             this.mapper = mapper;
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            GrpcChannel channel = GrpcChannel.ForAddress(configuration.GetValue<string>("GrpcSettings:ProfileServiceUrl"), new GrpcChannelOptions { HttpHandler = httpHandler });
+            client = new ProfileService.ProfileServiceClient(channel);
         }
 
         public async Task<ObjectResult> Handle(GetPaymentByIDQuery request, CancellationToken cancellationToken)
@@ -28,6 +34,16 @@ namespace Project.RefundService.Handlers.RefundHandlers
             {
                 var Payment = await paymentRepository.GetPayment(request.PaymentID);
                 var paymentDtos = mapper.Map<PaymentDetailDtos>(Payment);
+                var response = await client.GetProfileAsync(new GetProfileRequest { UserID = Payment.UserID.ToString() });
+                if (response.FirstName =="")
+                {
+                    return ApiResponse.NotFound("Get Payemnt Error");
+                }
+                paymentDtos.Author = new Author();
+                paymentDtos.Author.UserID = Guid.Parse(response.UserID);
+                paymentDtos.Author.FirstName = response.FirstName;
+                paymentDtos.Author.LastName = response.LastName;
+                paymentDtos.Author.Avatar = response.Avatar;
                 return ApiResponse.OK(paymentDtos);
             }
             catch (Exception ex)
--- Project.PaymentService/Model/PaymentDtos.cs
+++ Project.PaymentService/Model/PaymentDtos.cs
@@ -4,8 +4,6 @@
     {
         public Guid PaymentID { get; set; }
         public double PaymentAmount { get; set; }
-        public Guid UserID { get; set; }
-        public Guid BookingID { get; set; }
         public DateTime PaymentTime { get; set; }
         public string PaymentService { get; set; }
         public bool IsRefund { get; set; }
